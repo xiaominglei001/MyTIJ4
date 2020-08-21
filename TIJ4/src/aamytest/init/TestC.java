1b141a025993349c1330b882e62ae4049d4d15d6
xiaominglei001 <xiaominglei001@qq.com>
2020-08-21 14:13:24 +0800

    测试成员变量初始化等调用时机

---
+++ TIJ4/src/aamytest/init/TestC.java
@@ -0,0 +1,12 @@
+package aamytest.init;
+
+import aamytest.init.TestA;
+
+//注意：调用此处main不会打印信息
+public class TestC {
+
+    private TestA mA = new TestA("成员变量");
+    public static void main(String[] args) {
+
+    }
+}
